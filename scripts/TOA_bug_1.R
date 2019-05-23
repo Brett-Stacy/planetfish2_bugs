baceb3d31988ee0dbe59427dc42102d3f5d4ad93
Brett Stacy <45962770+Brett-Stacy@users.noreply.github.com>
2019-05-23 12:26:23 +1000

    first script

---
+++ scripts/TOA_bug_1.R
@@ -0,0 +1,9 @@
+## 23 May 2019 - R 3.5.3 - planetfish2 version 0.6.1
+
+## Let's find that bug!
+
+## Script for generating data and casal assessment output using Planetfish2
+## Goal: Use a scenario that performs poorly to inspect attributes between OM and AM that may cause discrepency. Namely, inspect 
+## perfect knowledge of population parameters, correspondence of catch quantity and survey scanns, etc. 
+
+
